73c999cc4ea01bf8f2ad26d62975f55866f40e8d
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2022-10-06 16:54:55 -0400

    Extend Tailwind Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,33 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
-    ],
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.jsx",
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
+  theme: {
+    extend: {
+      colors: {
+        amblack: "#171c1a",
+        amgrey: "#6e7a83",
+        amgreen: {
+          accent: "#49B265",
+          white: "#c2d6ca",
+          light: "#159947",
+          DEFAULT: "#1F5F5B",
+          dark: "#06373A",
+          black: "#062315",
         },
+      },
+      fontFamily: {
+        sans: ["Share Tech Mono", ...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 
-    plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")],
 };
