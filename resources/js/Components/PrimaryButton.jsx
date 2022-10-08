b08aec122ecf18e90a3378bcc178696de7524bff
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2022-10-08 01:39:46 -0400

    Restyle PrimaryButton to Theme

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -10,7 +10,7 @@ export default function PrimaryButton({
     <button
       type={type}
       className={
-        `inline-flex items-center px-4 py-2 bg-gray-900 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest active:bg-gray-900 transition ease-in-out duration-150 ${
+        `inline-flex items-center px-4 py-2 bg-amgreen-dark border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest active:bg-amgreen transition ease-in-out duration-150 ${
           processing && "opacity-25"
         } ` + className
       }
