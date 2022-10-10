dc04d44e8095053d3bbf84ed50c098e240c6f60d
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2022-10-10 07:57:11 -0400

    Add Update Method to ResponseMessage

--- app/Classes/ResponseMessage.php
+++ app/Classes/ResponseMessage.php
@@ -17,6 +17,14 @@ class ResponseMessage
     $this->extraData = $extraData;
   }
 
+  public function update(int $code, string $message, bool $isSuccess, $extraData = null)
+  {
+    $this->code = $code;
+    $this->message = $message;
+    $this->isSuccess = $isSuccess;
+    $this->extraData = $extraData;
+  }
+
   public function toJson()
   {
     return json_encode(array("status" => $this->code, "message" => $this->message, "success" => $this->isSuccess, "extraData" => $this->extraData));
